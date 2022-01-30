929598c988e1d7fb15c8316389f2d4cc1bc3b142
almaideen <almhdmaideen@gmail.com>
2022-01-30 08:32:18 +0530

    Errors Fixed

--- src/utils/model.py
+++ src/utils/model.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 
 def create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASS):
 
-    LAYERS =[tf.keras.layers.Flatten(input_shape=[28,28], name='Input Layer'),
-            tf.keras.layers.Dense(300,activation='relu', name='Hidden Layer1'),
-            tf.keras.layers.Dense(100,activation='relu', name='Hidden Layer2'),
-            tf.keras.layers.Dense(NUM_CLASS,activation='softmax',name = 'Ouput Layer')]
+    LAYERS =[tf.keras.layers.Flatten(input_shape=[28,28], name="InputLayer"),
+            tf.keras.layers.Dense(300,activation='relu', name="HiddenLayer1"),
+            tf.keras.layers.Dense(100,activation='relu', name="HiddenLayer2"),
+            tf.keras.layers.Dense(NUM_CLASS,activation='softmax',name = "OuputLayer")]
 
-    model_clf = tf.keras.model.Sequential(LAYERS)
+    model_clf = tf.keras.models.Sequential(LAYERS)
 
     model_clf.summary()
 
